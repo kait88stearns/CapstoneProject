b8b0f97020e43487b3c0eac578661d4226d805d7
kait88stearns <kaitlin.stearns@coloradocollege.edu>
2018-01-30 15:36:23 -0800

    scraper for reps

--- src/instagram_profile_scraper.py
+++ src/instagram_profile_scraper.py
@@ -2,10 +2,20 @@ import numpy as np
 import re 
 from selenium import webdriver
 import time 
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver import Firefox
+import pickle
 
 
 
 def login_insta(user, password):
+    '''
+    INPUT: user, password
+    OUTPUT: browser - headless firefox browser logged into instagram
+    '''
+    #options = Options()
+    #options.add_argument('-headless')
+    #browser = Firefox(executable_path='geckodriver', firefox_options=options)
     browser = webdriver.Firefox()
     time.sleep(3)
     # log into insta and go to profile
@@ -13,14 +23,20 @@ def login_insta(user, password):
     time.sleep(2)
     browser.find_elements_by_class_name('_f9sjj')[1].find_element_by_tag_name('a').click()
     time.sleep(2)
-    browser.find_element_by_class_name('_ev9xl').click()
+    #browser.find_element_by_class_name('_ev9xl').click()
     browser.find_element_by_class_name('_ev9xl').send_keys(user)
     browser.find_elements_by_class_name('_ev9xl')[1].click()
     browser.find_elements_by_class_name('_ev9xl')[1].send_keys(password)
     browser.find_element_by_tag_name('button').click()
     return browser
 
+
 def scrape_profile_info(browser, user_name):
+    '''
+    INPUT: browser - browser to user, logged into instagram
+           user_name - profile to scrape info for
+    OUTPUT: info - dictionary of scraped information       
+    '''
     # go to profile and click on followers
     browser.get("https://www.instagram.com/{}/".format(user_name))
     time.sleep(2)
@@ -33,10 +49,11 @@ def scrape_profile_info(browser, user_name):
     while len(followers) < num_followers:
         browser.execute_script("""flist= document.getElementsByClassName('_gs38e')[0];
     flist.scrollTop = flist.scrollTopMax; """)
+        time.sleep(1.1)
         follower_tags = browser.find_elements_by_class_name("_2nunc")
         for i in range(len(followers),len(follower_tags)):
             followers.append(follower_tags[i].find_element_by_tag_name('a').text)
-        time.sleep(np.random.normal(3, .2))
+        time.sleep(np.random.normal(3.5, .2))
         num_more -=10
         if num_more < -20:
             break
@@ -45,4 +62,15 @@ def scrape_profile_info(browser, user_name):
     caption = []
     for elem in browser.find_element_by_class_name("_tb97a").find_elements_by_xpath('*'):
         caption.append(elem.text)
-    return {'num_posts': num_posts, 'caption': caption, 'followers': followers, 'num_following':num_following, 'num_followers': num_followers}
+    info = {'num_posts': num_posts, 'caption': caption, 'followers': followers, 'num_following':num_following, 'num_followers': num_followers}
+    return info
+
+
+def make_pickle(to_pickle, pickle_name):
+    '''
+    INPUT: to_pickle - thing to pickle (like dictionary/dataframe)
+           pickle_name - what to call pickle file (ex. thing.pkl)       
+    '''
+    pickle_out = open(pickle_name,"wb")
+    pickle.dump(to_pickle, pickle_out)
+    pickle_out.close()
